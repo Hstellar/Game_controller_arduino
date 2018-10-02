2adb07c3180c7677fb6879a02e9222ed51500e20
Hstellar <henaghonia2015@gmail.com>
2018-10-02 17:29:03 +0530

    Add files via upload

---
+++ game_arduino.ino
@@ -0,0 +1,201 @@
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+===============================================
+*/
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+#include <Keypad.h>
+
+const byte numRows= 4; //number of rows on the keypad
+const byte numCols= 4; //number of columns on the keypad
+char keymap[numRows][numCols]=
+{
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {9,8,7,6}; //Rows 0 to 3
+byte colPins[numCols]= {5,4,3,2}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+int last_ay=0;
+// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
+// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
+// not so easy to parse, and slow(er) over UART.
+#define OUTPUT_READABLE_ACCELGYRO
+
+//#define DEBUG 
+
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)  Serial.print (x)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTLN(x)
+#endif
+
+#define USE_ACCEL 3
+#define USE_GYRO 3
+
+
+#define LED_PIN 13
+bool blinkState = false;
+
+uint16_t lastReport;
+
+const int numReadings = 25;
+
+#if defined(USE_ACCEL) && defined(USE_GYRO)
+const int numAxis = USE_ACCEL + USE_GYRO;
+const int AX = 0;
+const int AY = 1;
+const int AZ = 2;
+const int GX = 3;
+const int GY = 4;
+const int GZ = 5;
+#elif defined(USE_ACCEL)
+const int numAxis = USE_ACCEL;
+const int AX = 0;
+const int AY = 1;
+const int AZ = 2;
+#elif defined(USE_GYRO)
+const int numAxis = USE_GYRO;
+const int GX = 0;
+const int GY = 1;
+const int GZ = 2;
+#endif
+
+int32_t readings[numAxis][numReadings];  // the reading history
+int32_t readIndex[numAxis];              // the index of the current reading
+int32_t total[numAxis];                  // the running total
+int32_t average[numAxis];                // the average
+char serial_read;
+
+void setup() {
+  lastReport = millis();
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    Serial.begin(9600);
+    DEBUG_PRINTLN("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    accelgyro.setXGyroOffset(26);
+    accelgyro.setYGyroOffset(-7);
+    accelgyro.setZGyroOffset(38);
+    accelgyro.setXAccelOffset(-814);
+    accelgyro.setYAccelOffset(-63);
+    accelgyro.setZAccelOffset(1665); // 1688 factory default for my test chip
+
+    // verify connection
+    DEBUG_PRINTLN("Testing device connections...");
+    DEBUG_PRINTLN(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    // configure Arduino LED for
+    pinMode(LED_PIN, OUTPUT);
+    
+
+    // zero-fill all the arrays:
+    for (int axis = 0; axis < numAxis; axis++) {
+        readIndex[axis] = 0;
+        total[axis] = 0;
+        average[axis] = 0;
+        for (int i = 0; i<numReadings; i++){
+            readings[axis][i] = 0;
+        }
+    }
+}
+
+
+void loop() {
+    // read raw accel/gyro measurements from device
+if ( Serial.available() ){
+      serial_read=Serial.read();
+}
+accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+int ax_map=map(ax,-17000,17000,-100,100);
+//scale it to use it with the car speed
+  if(ax_map>-20&&ax_map<20)
+  ax_map=0;
+
+Serial.print(ax_map);
+Serial.print(",");
+last_ay=map(ay,-17000,17000,-100,100);  
+ if(last_ay>-30&&last_ay<30)
+  last_ay=0; 
+  Serial.print(last_ay); //scale it for the car rotation
+  char keypressed = myKeypad.getKey();
+  Serial.print(",");
+ if(keypressed)
+      Serial.println(keypressed);
+ else
+      Serial.println("0");
+delay(50);
+//    #endif
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+    serial_read=0;
+}
+void smooth(int axis, int32_t val) {
+    // pop and subtract the last reading:
+    total[axis] -= readings[axis][readIndex[axis]];
+    total[axis] += val;
+
+    // add value to running total
+    readings[axis][readIndex[axis]] = val;
+    readIndex[axis]++;
+
+    if(readIndex[axis] >= numReadings)
+        readIndex[axis] = 0;
+
+    // calculate the average:
+    average[axis] = total[axis] / numReadings;
+}
